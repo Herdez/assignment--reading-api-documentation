5e567eb9f2716b07ec32f496f1c43d163536fc9b
Herdez <abelquantikum@gmail.com>
2018-08-02 21:12:30 -0500

    Assignment Reading API Documentation v2.0

--- js/api-03-google-sunrise-darksky.js
+++ js/api-03-google-sunrise-darksky.js
@@ -22,14 +22,45 @@
 
 const answerElement_apimashup_1 = document.getElementById('apimashup-1')
 
+request.get('https://maps.googleapis.com/maps/api/geocode/json?address=Montreal')
+  .then(function(serverRes){
+    let apiJsonData = serverRes.body;
+    apiJsonData.results.forEach(function(type){
+    	let lat = type.geometry.location.lat;
+    	let lng = type.geometry.location.lng;
+    	answerElement_apimashup_1.innerHTML += '<p>Lat: ' + lat + '</p>' +
+    										   '<p>Lng: ' + lng + '</p>';
+    });
+  })
 
 
 
 //========================================================================
 //  (2) What time does the sun set in Montreal (based on the Google geocode coordinates)?
+function getSunset(latitude, longitude) {
 
-const answerElement_apimashup_2 = document.getElementById('apimashup-2')
+	const answerElement_apimashup_2 = document.getElementById('apimashup-2')
 
+    var API_URL = `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&date=today`;
+
+    request.get(API_URL)
+	  .then(function(response){
+		  let apiJsonData = response.body;
+		  let sunsetTime = apiJsonData.results.sunset;
+		  answerElement_apimashup_2.innerHTML += '<p>Sunset: ' + sunsetTime + '</p>';	
+		})
+}
+
+
+request.get('https://maps.googleapis.com/maps/api/geocode/json?address=Montreal')
+  .then(function(serverRes){
+    let apiJsonData = serverRes.body;
+    apiJsonData.results.forEach(function(type){
+    	let lat = type.geometry.location.lat;
+    	let lng = type.geometry.location.lng;
+    	getSunset(lat, lng);						   
+    });
+  })
 
 
 
@@ -38,8 +69,31 @@ const answerElement_apimashup_2 = document.getElementById('apimashup-2')
 //========================================================================
 //  (3) What is the weekly weather forecast in Montreal? (look for summary property in 'daily')
 
-const answerElement_apimashup_3 = document.getElementById('apimashup-3')
+function getWeather(latitude, longitude) {
 
+	const answerElement_apimashup_3 = document.getElementById('apimashup-3')
+
+	var API_KEY = '7b99d5e089197748e933189d8174655f';
+    var API_URL = `https://api.darksky.net/forecast/${API_KEY}/${latitude},${longitude}`;
+
+    request.get(API_URL)
+	  .then(function(response){
+		  let apiJsonData = response.body;
+		  let weather = apiJsonData.daily.summary;
+		  answerElement_apimashup_3.innerHTML = '<p>' + weather + '</p>';
+		})
+}
+
+request.get('https://maps.googleapis.com/maps/api/geocode/json?address=Montreal')
+  .then(function(serverRes){
+    let apiJsonData = serverRes.body;
+    apiJsonData.results.forEach(function(type){
+    	console.log(type.daily);
+    	var lat = type.geometry.location.lat;
+    	var lng = type.geometry.location.lng;
+    	getWeather(lat, lng);   
+    });
+  })
 
 
 
